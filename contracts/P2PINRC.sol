02b021df50c18c8c8886ce9402c22739cc434ecc
muskbuster <81789395+muskbuster@users.noreply.github.com>
2023-01-25 14:47:24 +0530

    Update P2PINRC.sol

--- contracts/P2PINRC.sol
+++ contracts/P2PINRC.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-contract EscrowERC is Initializable, UUPSUpgradeable, OwnableUpgradeable,PausableUpgradeable {
+contract EscrowINRC is Initializable, UUPSUpgradeable, OwnableUpgradeable,PausableUpgradeable {
 
      function initialize() public initializer {
        __UUPSUpgradeable_init();
