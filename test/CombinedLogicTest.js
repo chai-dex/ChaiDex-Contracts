df42353de06cdd674dcc17ca5af33713bd0e6bc4
muskbuster <sudeepkamat79@gmail.com>
2023-02-06 17:03:44 +0000

    tests not uploading

--- test/CombinedLogicTest.js
+++ test/CombinedLogicTest.js
@@ -4,7 +4,7 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 describe("Deployment and functioning of LP", function() {
     it('works', async () => {
         const [owner, addr1, addr2] = await ethers.getSigners();
-        
+
       const Liquidity = await ethers.getContractFactory("LiquidityPool");
       const Treasury = await ethers.getContractFactory("TreasuryPool");
       const Apy = await ethers.getContractFactory("Apy");
@@ -23,7 +23,7 @@ describe("Deployment and functioning of LP", function() {
 
     it('staking and unstaking with epoch over condition ', async () => {
         const [owner, addr1, addr2] = await ethers.getSigners();
-        
+
       const Liquidity = await ethers.getContractFactory("LiquidityPool");
     //   const DEX = await ethers.getContractFactory("htlc");
     //   const Treasury = await ethers.getContractFactory("TreasuryPool");
@@ -34,13 +34,13 @@ describe("Deployment and functioning of LP", function() {
     //   const instance2 = await upgrades.deployProxy(DEX, []);
     //   const instance3= await upgrades.deployProxy(Treasury, []);
       const instance4 = await USDmock.deploy();
-    
+
       console.log(instance.address);
       console.log(instance4.address);
       expect(await instance.setUSDAddress(0,"USDC",instance4.address,))
       expect(await instance4.approve(instance.address, 5000))
       expect(await instance.stake(0,1000))
-     
+
       await expect( instance.unstake(0,1000)).to.be.revertedWith('Treasury Pool: Amount is Zero or Greater than Stake').then(console.log("reverted as expected with amount incorrect error"));
       expect(await instance.setEpoch(true))
       expect(await instance.setUnstake(true))
@@ -55,9 +55,9 @@ describe("Deployment and functioning of LP", function() {
     //   console.log(instance4.allowance(instance.address));
     });
 
-    it('staking and unstaking with epoch not over condition and over condition Should revert with epoch not over', async () => {
+    it('staking and unstaking with epoch not over condition  Should revert with epoch not over', async () => {
         const [owner, addr1, addr2] = await ethers.getSigners();
-        
+
       const Liquidity = await ethers.getContractFactory("LiquidityPool");
     //   const DEX = await ethers.getContractFactory("htlc");
     //   const Treasury = await ethers.getContractFactory("TreasuryPool");
@@ -68,17 +68,17 @@ describe("Deployment and functioning of LP", function() {
     //   const instance2 = await upgrades.deployProxy(DEX, []);
     //   const instance3= await upgrades.deployProxy(Treasury, []);
       const instance4 = await USDmock.deploy();
-    
+
       console.log(instance.address);
       console.log(instance4.address);
       expect(await instance.setUSDAddress(0,"USDC",instance4.address,))
       expect(await instance4.approve(instance.address, 5000))
       expect(await instance.stake(0,1000))
       expect(await instance.setUnstake(true))
-      await expect( instance.unstake(0,1000)).to.be.revertedWith('Treasury Pool: Amount is Zero or Greater than Stake').then(console.log("reverted as expected with amount incorrect error"));
-       expect( await instance.unstake(0,700))
+      await  expect(  instance.unstake(0,1000)).to.be.revertedWith('Treasury Pool: Amount is Zero or Greater than Stake').then(console.log("reverted as expected with amount incorrect error"));
+      await  expect(  instance.unstake(0,700))
        console.log("unstaked 80% proceeding to unstake all");
-       await expect(  instance.unstakeAll(0)).to.be.revertedWith('epoch not over')
+       await  expect(  instance.unstakeAll(0)).to.be.revertedWith('epoch not over')
        console.log(" unstake all reverted as expected");
       var ownerBalance = await instance4.balanceOf(owner.address);
       var contBalance = await instance4.balanceOf(instance.address);
@@ -87,6 +87,44 @@ describe("Deployment and functioning of LP", function() {
     //   console.log(instance4.allowance(instance.address));
     });
 
-    it('')
+    it('pause and unpause for unstake', async () => {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+
+      const Liquidity = await ethers.getContractFactory("LiquidityPool");
+    //   const DEX = await ethers.getContractFactory("htlc");
+    //   const Treasury = await ethers.getContractFactory("TreasuryPool");
+    //   const Apy = await ethers.getContractFactory("Apy");
+      const USDmock = await ethers.getContractFactory("TESTUSD");
+
+      const instance = await upgrades.deployProxy(Liquidity, []);
+    //   const instance2 = await upgrades.deployProxy(DEX, []);
+    //   const instance3= await upgrades.deployProxy(Treasury, []);
+      const instance4 = await USDmock.deploy();
+
+      console.log(instance.address);
+      console.log(instance4.address);
+      expect(await instance.setUSDAddress(0,"USDC",instance4.address,))
+      expect(await instance4.approve(instance.address, 5000))
+      expect(await instance.stake(0,1000))
+      expect(await instance.setUnstake(true))
+      expect(await instance.setEpoch(true))
+      expect(await instance.pause())
+      await  expect(  instance.unstake(0,200)).to.be.revertedWith('Pausable: paused').then(console.log("reverted as expected with paused error"));
+      await  expect(  instance.unstakeAll(0)).to.be.revertedWith('Pausable: paused').then(console.log("reverted as expected with paused error"));
+      console.log("Unpausing");
+      expect(await instance.unpause())
+      console.log("Unpaused");
+      await  expect(  instance.unstake(0,700))
+      await expect( instance.unstakeAll(0))
+      console.log(" unstake all complete");
+      await expect( instance.unstakeAll(0)).to.be.revertedWith('Amount cannot be 0').then(console.log("reverted as expected with amount zero error"));
+      var ownerBalance = await instance4.balanceOf(owner.address);
+      var contBalance = await instance4.balanceOf(instance.address);
+      console.log(ownerBalance);
+      console.log(contBalance);
+
+      });
+      it
+
 
   });
